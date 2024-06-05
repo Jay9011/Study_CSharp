9babd281391e87aa53886cc675f4a97a8112eb5f
Jay9011 <whdtjq5904@gmail.com>
2024-06-05 19:15:22 +0900

    Enum

---
+++ _02_Enum/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _02_Enum
+{
+    internal class Program
+    {
+        public enum DaysOfWeek
+        {
+            Sunday = 1,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 7; i++)
+            {
+                Console.WriteLine($"{(DaysOfWeek)i} = {i}");
+            }
+        }
+    }
+}
