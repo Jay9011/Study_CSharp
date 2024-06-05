b746a170d37af68e26c498a83b2693f2682152da
Jay9011 <whdtjq5904@gmail.com>
2024-06-05 18:46:43 +0900

    Nullable

--- _01_Datas/Program.cs
+++ _01_Datas/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // int? nullableInt = 5;
+            // double? nullableDouble = 3.14;
+            // bool? nullableBool = null;
+            //
+            // Console.WriteLine($"NullableDouble: {nullableDouble.Value}");   // NullableDouble: 3.14
+            // if (nullableBool == null)
+            // {
+            //     Console.WriteLine("Null");
+            // }
+            //
+            // if (nullableInt.HasValue)
+            // {
+            //     Console.WriteLine($"Value: {nullableInt.Value}");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("No value");
+            // }
+
+
+
+            int? nullableInt = null;
+            int defaultValue = nullableInt ?? -1; // nullableInt가 null이면 -1을 사용
+            Console.WriteLine(defaultValue); // 출력: -1
+
+
+            int? length = nullableInt?.ToString().Length;
+            Console.WriteLine(length); // nullableInt가 null이면 length도 null
+
         }
     }
 }
