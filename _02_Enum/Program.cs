fc49e06ca7949316b12ddbd3c980b35c585f961a
Jay9011 <whdtjq5904@gmail.com>
2024-06-05 19:44:28 +0900

    Enum 수정

--- _02_Enum/Program.cs
+++ _02_Enum/Program.cs
@@ -15,10 +15,60 @@
 
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 7; i++)
+            // for (int i = 1; i <= 7; i++)
+            // {
+            //     Console.WriteLine($"{(DaysOfWeek)i} = {i}");
+            // }
+
+            // 문자열을 열거형으로 변환
+            string dayString = "Friday";
+            DaysOfWeek day = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), dayString);
+            Console.WriteLine(day); // 출력: Friday
+
+            // 열거형의 모든 값 가져오기
+            DaysOfWeek[] days = (DaysOfWeek[])Enum.GetValues(typeof(DaysOfWeek));
+            foreach (DaysOfWeek d in days)
             {
-                Console.WriteLine($"{(DaysOfWeek)i} = {i}");
+                Console.WriteLine(d);
             }
+
+            // 열거형의 모든 이름 가져오기
+            string[] dayNames = Enum.GetNames(typeof(DaysOfWeek));
+            foreach (string name in dayNames)
+            {
+                Console.WriteLine(name);
+            }
+
+
+            // DaysOfWeek input = (DaysOfWeek)Convert.ToInt32(Console.ReadLine());
+            //
+            // switch (input)
+            // {
+            //     case DaysOfWeek.Sunday:
+            //         Console.WriteLine("일요일");
+            //         break;
+            //     case DaysOfWeek.Monday:
+            //         Console.WriteLine("월요일");
+            //         break;
+            //     case DaysOfWeek.Tuesday:
+            //         Console.WriteLine("화요일");
+            //         break;
+            //     case DaysOfWeek.Wednesday:
+            //         Console.WriteLine("수요일");
+            //         break;
+            //     case DaysOfWeek.Thursday:
+            //         Console.WriteLine("목요일");
+            //         break;
+            //     case DaysOfWeek.Friday:
+            //         Console.WriteLine("금요일");
+            //         break;
+            //     case DaysOfWeek.Saturday:
+            //         Console.WriteLine("토요일");
+            //         break;
+            //     default:
+            //         Console.WriteLine("잘못된 입력");
+            //         break;
+            // }
         }
     }
 }
