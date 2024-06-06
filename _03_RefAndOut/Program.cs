b0d45f8d72882bb1678d2ed6df60c3fa941c3a9f
Jay9011 <whdtjq5904@gmail.com>
2024-06-06 16:38:31 +0900

    ref

---
+++ _03_RefAndOut/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _03_RefAndOut
+{
+    internal class Program
+    {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Add(int a, int b, out int result)
+        {
+            result = a + b;
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 20;
+
+            Swap(ref num1, ref num2);
+        }
+    }
+}
