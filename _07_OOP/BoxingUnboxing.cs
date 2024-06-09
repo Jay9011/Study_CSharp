394f1e5eb0da4f68909fe0d5d61decee034226c3
Jay9011 <whdtjq5904@gmail.com>
2024-06-09 22:14:38 +0900

    Boxing과 Unboxing

---
+++ _07_OOP/BoxingUnboxing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_OOP
+{
+    public class BoxingUnboxing
+    {
+        public static void Test()
+        {
+            ArrayList list = new ArrayList();
+
+            object[] array = new object[3];
+            array[0] = 123;
+            array[1] = "test";
+            array[2] = false;
+
+            List<int> listInt = new List<int>();
+        }
+    }
+}
