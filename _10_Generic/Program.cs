e2ebfa19541f6ff18d4131bb1b6d8a6b58e9c8e6
Jay9011 <whdtjq5904@gmail.com>
2024-06-20 16:58:57 +0900

    Generic

---
+++ _10_Generic/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+namespace _10_Generic
+{
+    public class BaseClass
+    {}
+    public class DerivedClass : BaseClass
+    {}
+
+    public static class Utilities
+    {
+        public static T Max<T>(T a, T b) where T : IComparable<T>
+        {
+            return a.CompareTo(b) >= 0 ? a : b;
+        }
+        public static B Allocate<B, D>() where D : B, new()
+        {
+            return new D();
+        }
+    }
+    public class Dict<K, V> where K : ICollection
+                            where V : IComparable
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass myClass = Utilities.Allocate<BaseClass, DerivedClass>();
+        }
+    }
+}
