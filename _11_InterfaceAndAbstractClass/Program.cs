4d6743da6284d8740dbef6582124a4e2199efc31
Jay9011 <whdtjq5904@gmail.com>
2024-06-21 22:57:05 +0900

    델리게이트

---
+++ _11_InterfaceAndAbstractClass/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _11_InterfaceAndAbstractClass
+{
+    public interface IMyInterface
+    {
+        void MyMethod();
+        void MyMethod2()
+        {
+            Console.WriteLine("기본 구현 제공");
+        }
+        string MyProperty { get; set; }
+        delegate void MyDelegate();
+        event MyDelegate MyEvent;
+        int this[int index] { get; set; }
+    }
+    public abstract class MyAbstractClass
+    {
+        public int index;
+        public abstract void MyMethod();
+    }
+    public class MyClass : IMyInterface
+    {
+        private char[] txtNumber;
+
+        public MyClass(int number)
+        {
+            txtNumber = number.ToString().ToCharArray();
+        }
+
+        public void MyMethod()
+        {
+            Console.WriteLine("MyMethod");
+        }
+        public string MyProperty { get; set; }
+        public event IMyInterface.MyDelegate MyEvent;
+        public int this[int index]
+        {
+            get { return int.Parse(txtNumber[index].ToString()); }
+            set { txtNumber[index] = value.ToString()[0]; }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass(123);
+            myClass.MyMethod2();
+        }
+    }
+}
---
+++ _12_Delegate/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _12_Delegate
+{
+    public delegate bool ActionBtnDelegate();
+
+    class DelegateTest
+    {
+        public ActionBtnDelegate actionBtnDelegates;
+
+        public ActionBtnDelegate GetNowOperableDelegate()
+        {
+            return actionBtnDelegates;
+        }
+        public void SetActionDelegate(ActionBtnDelegate actionBtnDelegate)
+        {
+            actionBtnDelegates = actionBtnDelegate;
+        }
+        public void Action()
+        {
+            if (actionBtnDelegates != null)
+            {
+                actionBtnDelegates();
+            }
+            else
+            {
+                Console.WriteLine("등록된 델리게이트가 없습니다.");
+            }
+        }
+    }
+
+    internal class Program
+    {
+        private static bool ActionBtn1()
+        {
+            Console.WriteLine("ActionBtn1");
+            return true;
+        }
+        private static bool ActionBtn2() {
+            Console.WriteLine("ActionBtn2");
+            return true;
+        }
+        
+        static void Main(string[] args)
+        {
+            ActionBtnDelegate actionBtnDelegate1 = new ActionBtnDelegate(ActionBtn1);
+            ActionBtnDelegate actionBtnDelegate2 = ActionBtn2;
+
+            DelegateTest delegateTest = new DelegateTest();
+            // delegateTest.SetActionDelegate(actionBtnDelegate1);
+            // ActionBtnDelegate actionBtnDelegate = delegateTest.GetNowOperableDelegate();
+            // if (actionBtnDelegate != null)
+            // {
+            //     Console.WriteLine($"Now Action : {actionBtnDelegate.ToString()}");
+            //     // Now Action : _12_Delegate.ActionBtnDelegate
+            // }
+            // delegateTest.Action(); // ActionBtn1
+            
+            ActionBtnDelegate test = ActionBtn1;
+            test = test + ActionBtn2; // test += ActionBtn2;
+            test();
+            // ActionBtn1
+            // ActionBtn2
+        }
+    }
+}
