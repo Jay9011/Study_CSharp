66b753c0a5f22bf0072552112e30f70de5b847a3
Jay9011 <whdtjq5904@gmail.com>
2024-06-22 19:52:48 +0900

    익명 델리게이트, Func, Action

---
+++ _12_Delegate/PlayerWEvent.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Delegate
+{
+    internal class MyEventArgs : EventArgs
+    {
+        public MyEventArgs(int n)
+        {
+            RemainStamina = n;
+        }
+        public int RemainStamina { get; set; }
+    }
+    internal class PlayerWEvent
+    {
+        public event EventHandler<MyEventArgs> actionBtnDelegates;
+
+        private StaminaWArgs stamina;
+
+        public PlayerWEvent()
+        {
+            stamina = new StaminaWArgs();
+            actionBtnDelegates += stamina.UseStamina;
+        }
+
+        public void Action()
+        {
+            actionBtnDelegates?.Invoke(this, new MyEventArgs(stamina.NowStamina));
+        }
+    }
+    internal class StaminaWArgs
+    {
+        public int NowStamina { get; set; } = 10;
+        public void UseStamina(object? sender, MyEventArgs e)
+        {
+            NowStamina -= 10;
+            Console.WriteLine("스태미너를 사용합니다.");
+        }
+    }
+    internal class DoorWArgs
+    {
+        public void Open(object? sender, MyEventArgs e)
+        {
+            if (e.RemainStamina < 10)
+            {
+                Console.WriteLine("스태미너가 부족합니다.");
+                return;
+            }
+            Console.WriteLine("문을 엽니다.");
+            return;
+        }
+    }
+}
--- _12_Delegate/Program.cs
+++ _12_Delegate/Program.cs
@@ -2,6 +2,38 @@
 {
     // public delegate bool ActionBtnDelegate();
 
+    // public delegate bool Predicate<T>(T x);
+
+    class TestClass
+    {
+        private Action ActionDelegate;
+        private Func<int, int, int> AddDelegate;
+        public void AddAction(Action action)
+        {
+            ActionDelegate += action;
+        }
+        public void OnAction()
+        {
+            ActionDelegate?.Invoke();
+        }
+    }
+
+    static class Utilities
+    {
+        public static int Count<T>(IEnumerable<T> items, Func<T, bool> predMethod)
+        {
+            int count = 0;
+            foreach (var item in items)
+            {
+                if (predMethod(item))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
     class DelegateTest
     {
         private ActionBtnDelegate actionBtnDelegates;
@@ -47,6 +79,15 @@
             Console.WriteLine("ActionBtn2");
             return true;
         }
+
+        private static void ActionTest()
+        {
+            Console.WriteLine("ActionTest");
+        }
+        private static int AddTest(int x, int y)
+        {
+            return x + y;
+        }
         
         static void Main(string[] args)
         {
@@ -86,19 +127,51 @@
             // player.actionBtnDelegates = item.PickUp;
             // player.actionBtnDelegates.Invoke(); // 아이템을 줍습니다.
 
-            Player player = new Player();
-            Stamina stamina = new Stamina();
-            player.AddAction(stamina.UseStamina);
+            // Player player = new Player();
+            // Stamina stamina = new Stamina();
+            // player.AddAction(stamina.UseStamina);
+            //
+            // Door door = new Door();
+            // player.AddAction(door.Open);
+            // player.OnAction();
+            // // 스태미너를 사용합니다.
+            // // 문을 엽니다.
+            //
+            // player.RemoveAction(door.Open);
+            // player.OnAction();
+            // // 스태미너를 사용합니다.
 
-            Door door = new Door();
-            player.AddAction(door.Open);
-            player.OnAction();
-            // 스태미너를 사용합니다.
-            // 문을 엽니다.
+            // PlayerWEvent playerWEvent = new PlayerWEvent();
+            //
+            // DoorWArgs door = new DoorWArgs();
+            // playerWEvent.actionBtnDelegates += door.Open;
+            //
+            // playerWEvent.Action();
+            // // 스태미너를 사용합니다.
+            // // 문을 엽니다.
+            //     
+            // playerWEvent.Action();
+            // // 스태미너를 사용합니다.
+            // // 스태미너가 부족합니다.
 
-            player.RemoveAction(door.Open);
-            player.OnAction();
-            // 스태미너를 사용합니다.
+            var numbers = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            int oddNumbers = Utilities.Count(numbers, delegate(int n) { return n % 2 != 0; });
+            Console.WriteLine(oddNumbers); // 5
+
+            int evenNumbers = Utilities.Count(numbers, delegate(int n) { return n % 2 == 0; });
+            Console.WriteLine(evenNumbers); // 4
+
+            Func<int, int, int> add = delegate(int x, int y) { return x + y; };
+            Console.WriteLine( add(3, 4) ); // 7
+
+            Action<int> print = delegate(int x) { Console.WriteLine(x); };
+            print(10); // 10
+
+
+            TestClass testClass = new TestClass();
+            testClass.AddAction(ActionTest);
+            testClass.OnAction(); // ActionTest
         }
     }
 }
