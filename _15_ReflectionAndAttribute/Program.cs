d91863f13f251ac62fc8043bac3e92ca44a18128
Jay9011 <whdtjq5904@gmail.com>
2024-06-23 20:02:41 +0900

    Reflection

---
+++ _15_ReflectionAndAttribute/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Reflection;
+
+namespace _15_ReflectionAndAttribute
+{
+    public class CustomAttribute : System.Attribute
+    {
+        public string Description { get; set; }
+        public CustomAttribute(string description)
+        {
+            Description = description;
+        }
+    }
+    [Custom("Reflection 어셈블리 파일의 TestClass")]
+    internal class Program_TestClass
+    {
+        private int PrivateInt;
+        protected double ProtectedDouble;
+        public string PublicString;
+
+        private void PrivateMethod()
+        {
+            Console.WriteLine("Program Private Method 호출");
+        }
+    }
+
+    internal class Program
+    {
+        static string GetAccessName(FieldInfo field)
+        {
+            if (field.IsPublic)
+            {
+                return "Public";
+            }
+            else if (field.IsPrivate)
+            {
+                return "Private";
+            }
+            else if (field.IsFamily)
+            {
+                return "Protected";
+            }
+            else
+            {
+                return "Internal";
+            }
+        }
+        static void TypeCheck(Type type)
+        {
+            // 해당 타입의 모든 필드 정보를 얻어온다.
+            FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+            foreach (FieldInfo field in fields)
+            {
+                Console.WriteLine($"{GetAccessName(field)} {field.FieldType.Name} {field.Name}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ReflectionClass.Run();
+            /* -- 실행 결과 --
+             * EmbeddedAttribute        ──┐
+             * NullableAttribute          │
+             * NullableContextAttribute   │  Reflection 어셈블리 파일
+             * Program_TestClass          │
+             * Program                    │
+             * ReflectionClass          ──┘
+             */
+
+            /*
+             * EmbeddedAttribute        ──┐
+             * NullableAttribute          │
+             * NullableContextAttribute   │  Predicate 어셈블리 파일
+             * Utilities                  │
+             * Program                    │
+             * <>c                      ──┘
+             */
+
+            // Program_TestClass test = new Program_TestClass();
+            // TypeCheck(test.GetType());
+            /*
+             * Private Int32 PrivateInt
+             * Protected Double ProtectedDouble
+             * Public String PublicString
+             */
+
+            // object? DynamicInstance = Activator.CreateInstance(typeof(Program_TestClass));
+            // Console.WriteLine($"{DynamicInstance.GetType().Name}");
+            /* -- 실행 결과 --
+             * Program_TestClass
+             */
+
+            // MethodInfo? method = DynamicInstance.GetType().GetMethod("PrivateMethod", BindingFlags.NonPublic | BindingFlags.Instance);
+            // method?.Invoke(DynamicInstance, null);
+            /* -- 실행 결과 --
+             * Program Private Method 호출
+             */
+
+            Type type = typeof(Program_TestClass);
+            object[] attributes = type.GetCustomAttributes(typeof(CustomAttribute), true);
+            foreach (CustomAttribute attribute in attributes)
+            {
+                Console.WriteLine(attribute.Description);
+            }
+            /* -- 실행 결과 --
+             * Reflection 어셈블리 파일의 TestClass
+             */
+        }
+    }
+}
---
+++ _15_ReflectionAndAttribute/Reflection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_ReflectionAndAttribute
+{
+    public static class ReflectionClass
+    {
+        public static void Run()
+        {
+            // Assembly assembly = Assembly.GetExecutingAssembly();
+            //
+            // foreach (Type type in assembly.GetTypes())
+            // {
+            //     Console.WriteLine(type.Name);
+            // }
+
+            // string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            // string solutionDirectory = Directory.GetParent(baseDirectory).Parent.Parent.Parent.Parent.FullName;
+            // string path = solutionDirectory + @"\_14_Predicate\bin\Debug\net6.0\_14_Predicate.dll";
+            //
+            // Assembly predicateAssembly = Assembly.LoadFrom(path);
+            //
+            // foreach (Type type in predicateAssembly.GetTypes())
+            // {
+            //     Console.WriteLine(type.Name);
+            // }
+
+            // Module[] modules = Assembly.GetExecutingAssembly().GetModules();
+            // foreach (Module module in modules)
+            // {
+            //     Console.WriteLine(module.Name);
+            // }
+        }
+    }
+}
