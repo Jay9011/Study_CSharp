456eced7703c0f45f492a509c3fcf0337f520ea1
Jay9011 <whdtjq5904@gmail.com>
2024-06-26 15:15:30 +0900

    확장 메서드

---
+++ _16_ExtensionMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _16_ExtensionMethod
+{
+    // class NewString : String    // sealed 형식 'string'에서 파생될 수 없습니다.
+    // {
+    //
+    // }
+
+    static class ExtendString
+    {
+        public static void CustomMethod(this string str)
+        {
+            Console.WriteLine($"Custom: {str}");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            "Hello".CustomMethod(); // Custom: Hello
+        }
+    }
+}
