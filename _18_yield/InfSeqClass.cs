9a9993af7ffaf550bf64e90de0cca6dd42439096
Jay9011 <whdtjq5904@gmail.com>
2024-08-19 13:17:25 +0900

    yield 키워드 테스트

---
+++ _18_yield/InfSeqClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_yield
+{
+    internal class InfSeqClass
+    {
+        public static IEnumerable<int> InfiniteSequence()
+        {
+            int i = 0;
+            while (true)
+            {
+                yield return i++;
+            }
+        }
+        public static void Exmaple()
+        {
+            var iterator = InfiniteSequence().GetEnumerator();
+
+            Console.WriteLine(iterator.Current); // 아직 시작 전이라 초기 상태
+            iterator.MoveNext();
+            Console.WriteLine(iterator.Current); // 0
+            iterator.MoveNext();
+            Console.WriteLine(iterator.Current); // 1
+            iterator.MoveNext();
+            Console.WriteLine(iterator.Current); // 2
+
+            // 두 번째 반복자 생성
+            var newIterator = InfiniteSequence().GetEnumerator();
+            newIterator.MoveNext();
+            Console.WriteLine(newIterator.Current); // 0, 새로운 반복자이므로 처음부터 시작
+            newIterator.MoveNext();
+            Console.WriteLine(newIterator.Current); // 1
+        }
+    }
+}
---
+++ _18_yield/Program.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+
+namespace _18_yield
+{
+    // 상태 기계 예시
+    public class GetNumbersEnumerable : IEnumerable<int>
+    {
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new GetNumbersEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public static IEnumerable<int> Create()
+        {
+            return new GetNumbersEnumerable();
+        }
+    }
+
+    public class GetNumbersEnumerator : IEnumerator<int>
+    {
+        private int state = 0;
+        private int current;
+
+        public int Current => current;
+
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {
+            switch (state)
+            {
+                case 0:
+                    current = 1;
+                    state = 1;
+                    return true;
+                case 1:
+                    current = 2;
+                    state = 2;
+                    return true;
+                case 2:
+                    current = 3;
+                    state = -1;  // End of enumeration
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        public void Reset()
+        {
+            state = 0;
+        }
+
+        public void Dispose() { }
+    }
+
+    public static class Example01
+    {
+        public static IEnumerable<int> GetNumbers()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+        }
+        public static void Example()
+        {
+            foreach (var number in GetNumbers())
+            {
+                Console.WriteLine(number);  // 출력: 1, 2, 3
+            }
+        }
+    }
+
+    public static class Example02
+    {
+        public static void Example()
+        {
+            foreach (var number in GetNumbersEnumerable.Create())
+            {
+                Console.WriteLine(number);  // 출력: 1, 2, 3
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InfSeqClass.Exmaple();
+        }
+    }
+}
