b9abeefff10c887502899fd3aaa4fcf22e224d44
Jay9011 <whdtjq5904@gmail.com>
2024-09-19 15:36:48 +0900

    팩토리 패턴

---
+++ _20_Pattern_Factory/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _20_Pattern_Factory
+{
+    public interface IProduct
+    {
+        void DoStuff();
+    }
+    public class ProductA : IProduct
+    {
+        public void DoStuff()
+        {
+            Console.WriteLine("ProductA");
+        }
+    }
+    public class ProductB : IProduct
+    {
+        public void DoStuff()
+        {
+            Console.WriteLine("ProductB");
+        }
+    }
+    public interface IFactory
+    {
+        IProduct CreateProduct();
+    }
+    public class FactoryA : IFactory
+    {
+        public IProduct CreateProduct()
+        {
+            return new ProductA();
+        }
+    }
+    public class FactoryB : IFactory
+    {
+        public IProduct CreateProduct()
+        {
+            return new ProductB();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IFactory factory = new FactoryA();
+            IProduct product = factory.CreateProduct();
+            product.DoStuff();
+        }
+    }
+}
