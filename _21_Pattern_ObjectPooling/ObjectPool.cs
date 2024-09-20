da4bbfcb3bec9b526317d2bf13ffac488f4b7612
Jay9011 <whdtjq5904@gmail.com>
2024-09-20 14:24:53 +0900

    Object Pooling 패턴

---
+++ _21_Pattern_ObjectPooling/ObjectPool.cs
@@ -0,0 +1,59 @@
+﻿namespace _21_Pattern_ObjectPooling;
+
+// 오브젝트 클래스
+public class PoolObject
+{
+    public bool IsActive { get; set; }
+
+    public PoolObject()
+    {
+        IsActive = false;
+    }
+}
+
+// 오브젝트 풀 클래스
+public class ObjectPool<T> where T : PoolObject, new()
+{
+    private readonly List<T> _pool;
+    private readonly int _maxSize;
+
+    public ObjectPool(int maxSize = 10)
+    {
+        _pool = new List<T>(maxSize);
+        _maxSize = maxSize;
+        for (int i = 0; i < maxSize; i++)
+        {
+            _pool.Add(new T());
+        }
+    }
+    public T Get(int index)
+    {
+        return _pool[index];
+    }
+
+    public T Use()
+    {
+        for (int i = 0; i < _pool.Count; i++)
+        {
+            if (!_pool[i].IsActive)
+            {
+                _pool[i].IsActive = true;
+                return _pool[i];
+            }
+        }
+
+        return null;    // 모든 오브젝트가 사용중이면 null 반환
+    }
+
+    public void Release(T obj)
+    {
+        if (_pool.Contains(obj))
+        {
+            obj.IsActive = false;
+        }
+    }
+
+    public int ActiveCount => _pool.FindAll(obj => obj.IsActive).Count;
+    public int InUseCount => _maxSize - ActiveCount;
+    public int Capacity => _maxSize;
+}
---
+++ _21_Pattern_ObjectPooling/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace _21_Pattern_ObjectPooling
+{
+    public class MyObject: PoolObject
+    {
+        // MyObject에 추가할 필드나 메서드를 정의합니다.
+        public string Name { get; set; }
+    }
+    internal class Program
+    {
+        private static ObjectPool<MyObject> objectPool;
+
+        static void Main(string[] args)
+        {
+            objectPool = new ObjectPool<MyObject>(10);
+
+            PrintObjectStatus();
+
+            while (true)
+            {
+                var key = Console.ReadKey().Key;
+                if (key == ConsoleKey.C)
+                {
+                    ActiveObject();
+                }
+                else if (key == ConsoleKey.Q) 
+                {
+                    break;
+                }
+            }
+        }
+
+        private static void ActiveObject()
+        {
+            var obj = objectPool.Use();
+            if (obj != null)
+            {
+                PrintObjectStatus();
+
+                Task.Run(async () =>
+                {
+                    await Task.Delay(2000);
+                    DeactiveObject(obj);
+                });
+            }
+            else
+            {
+                Console.WriteLine("\nObject pool is full.");
+            }
+        }
+
+        private static void DeactiveObject(MyObject obj)
+        {
+            objectPool.Release(obj);
+            PrintObjectStatus();
+        }
+
+        private static void PrintObjectStatus()
+        {
+            Console.Clear();
+            Console.WriteLine("Press 'C' to create object, 'Q' to quit");
+            Console.WriteLine("\nObject Pool Status:");
+            for (int i = 0; i < objectPool.Capacity; i++)
+            {
+                PoolObject obj = objectPool.Get(i);
+                Console.Write($"Object {obj.GetHashCode(), 10} is active: ");
+                if (obj.IsActive)
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                Console.Write($"{obj.IsActive}");
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+        }
+    }
+}
